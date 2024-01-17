26bd42cff044c2e7bb5055168528ae5844667ee8
Emanuel Ruiz <ruiz.e.642@gmail.com>
2024-01-17 19:57:38 -0300

    implemented navbar reacts to user login or logout

--- src/app/core/components/navbar/navbar.component.ts
+++ src/app/core/components/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from 'src/app/features/auth/models/user.model';
 import { AuthService } from 'src/app/features/auth/services/auth.service';
 
 @Component({
@@ -7,16 +9,23 @@ import { AuthService } from 'src/app/features/auth/services/auth.service';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  user?: User;
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit(): void {
     this.authService.user()
     .subscribe({
       next: (response) => {
-        
+        this.user = response;
       }
-    })
+    });
+    this.user = this.authService.getUser();
+  }
+
+  onLogout(): void {
+    this.authService.logout();
+    this.router.navigateByUrl('/');
   }
 
 }
